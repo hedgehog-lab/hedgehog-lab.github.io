cd0a4e84f4b7eb5e004736e6db4ddd6db6172d3c
lidang <wxr001@gmail.com>
2020-06-07 21:28:08 -0500

    version 0.1

---
+++ precache-manifest.89a0fefa43b6cfd977b0c6ef95846e30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70a98106a9127610ab7d00c8f279de6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fa4cd1803f76330eead",
+    "url": "/static/css/main.ba0b0a3f.chunk.css"
+  },
+  {
+    "revision": "4e74a3b36ecb4dcb9766",
+    "url": "/static/js/2.03ec28df.chunk.js"
+  },
+  {
+    "revision": "895ff4714d59b5e28fd05e1113b233b5",
+    "url": "/static/js/2.03ec28df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fa4cd1803f76330eead",
+    "url": "/static/js/main.0f076da8.chunk.js"
+  },
+  {
+    "revision": "b5c19cb19b9dd93b380b",
+    "url": "/static/js/runtime-main.549ce1c2.js"
+  }
+]);
